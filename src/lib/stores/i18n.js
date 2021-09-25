df3a62b4e56aefc21fccb77aec68f4089d0e835a
sucram <marcusfarstad@protonmail.ch>
2021-09-25 22:34:27 +0300

    quick fix to translation lack of key

--- src/lib/stores/i18n.js
+++ src/lib/stores/i18n.js
@@ -10,7 +10,10 @@ function translate(locale, key, vars) {
   // Let's throw some errors if we're trying to use keys/locales that don't exist.
   // We could improve this by using Typescript and/or fallback values.
   if (!key) throw new Error("no key provided to $t()");
-  if (!locale) throw new Error(`no translation for key "${key}"`);
+  if (!locale) {
+    locale = "fa"
+  }
+  // if (!locale) throw new Error(`no translation for key "${key}"`);
 
   // Grab the translation from the translations object.
   let text = translations[locale][key];
